49d57ad219568c3883bbdbdc5818a2575110c6df
Albert Salim <albertlimca@gmail.com>
2022-05-22 14:16:33 +0800

    Return a hash with items for Alfred

--- brave_alfred.rb
+++ brave_alfred.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env ruby
 
+require 'json'
+
 class BraveAlfred
   def initialize(home = ENV['HOME'])
     @home = home
   end
 
   def execute
-    ''
+    {
+      items: []
+    }
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  puts BraveAlfred.new.execute
+  puts BraveAlfred.new.execute.to_json
 end

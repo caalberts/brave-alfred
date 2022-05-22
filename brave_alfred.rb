efab333133c05d7881bec9fe433c261ce062a7a8
Albert Salim <albertlimca@gmail.com>
2022-05-22 15:24:55 +0800

    Generate CLI launcher with profile directory

--- brave_alfred.rb
+++ brave_alfred.rb
@@ -5,6 +5,7 @@ require 'json'
 class BraveAlfred
   APPLICATION_SUPPORT_PATH = 'Library/Application Support/BraveSoftware/Brave-Browser'
   PREFERENCES_FILE = 'Preferences'
+  EXECUTABLE = '"/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"'
 
   def initialize(home = ENV['HOME'])
     @home = home
@@ -12,8 +13,53 @@ class BraveAlfred
 
   def execute
     {
-      items: []
+      items: create_items
     }
+  end
+
+  private
+
+  attr_reader :home
+
+  def create_items
+    profiles.map do |profile|
+      {
+        title: profile.name,
+        arg: launcher_for(profile)
+      }
+    end
+  end
+
+  def profiles
+    glob_pattern = "#{home}/#{APPLICATION_SUPPORT_PATH}/*/#{PREFERENCES_FILE}"
+
+    Dir.glob(glob_pattern)
+       .reject { |path| path.include?('System Profile') }
+       .map { |preference_path| Profile.from(preference_path) }
+  end
+
+  def launcher_for(profile)
+    "#{EXECUTABLE} --profile-directory=\"#{profile.directory}\""
+  end
+
+  Profile = Struct.new(:name, :directory, keyword_init: true) do
+    GENERATED_PROFILES_MATCHER = %r{#{APPLICATION_SUPPORT_PATH}/(Default|Guest Profile|Profile [0-9]+)/#{PREFERENCES_FILE}}
+
+    def self.from(preference_path)
+      directory = GENERATED_PROFILES_MATCHER.match(preference_path)[1]
+
+      raise ArgumentError if directory.nil?
+
+      self.new(name: parse_name(preference_path), directory: directory)
+    end
+
+    def self.parse_name(preference_path)
+      file = File.open(preference_path)
+
+      JSON.load(file).dig('profile', 'name')
+    ensure
+      file.close
+    end
   end
 end
 
--- brave_alfred_spec.rb
+++ brave_alfred_spec.rb
@@ -4,7 +4,7 @@ require_relative './brave_alfred'
 
 class BraveAlfredTest < Minitest::Test
   def test_execute_returns_a_hash_with_items_array
-    with_stub_home do |home|
+    with_stub_profiles_in_home do |home|
       result = BraveAlfred.new(home).execute
 
       assert_instance_of(Hash, result)
@@ -12,30 +12,78 @@ class BraveAlfredTest < Minitest::Test
     end
   end
 
+  def test_execute_returns_as_many_items_as_there_are_profiles
+    with_stub_profiles_in_home do |home|
+      result = BraveAlfred.new(home).execute
+      items = result[:items]
+
+      assert_equal(test_profiles.length, items.length)
+    end
+  end
+
+  def test_execute_returns_items_with_brave_cli_launcher
+    with_stub_profiles_in_home do |home|
+      result = BraveAlfred.new(home).execute
+      items = result[:items]
+
+      test_profiles.each do |directory, name|
+        expected_cmd = "#{BraveAlfred::EXECUTABLE} --profile-directory=\"#{directory}\""
+
+        assert_includes(items, { title: name, arg: expected_cmd })
+      end
+    end
+  end
+
+  def test_execute_does_not_include_system_profile
+    with_stub_profiles_in_home({ 'System Profile' => 'System'}) do |home|
+      result = BraveAlfred.new(home).execute
+      items = result[:items]
+
+      assert_empty(items)
+    end
+  end
+
   private
 
-  def with_stub_home
+  def with_stub_profiles_in_home(profiles = test_profiles)
     Dir.mktmpdir do |dir|
-      stub_brave_support_dir(dir)
+      stub_brave_support_dir(dir, profiles)
       yield dir
     end
   end
 
-  def stub_brave_support_dir(home_dir)
+  def stub_brave_support_dir(home_dir, profiles)
     support_dir = File.join(home_dir, BraveAlfred::APPLICATION_SUPPORT_PATH)
 
     FileUtils.mkdir_p(support_dir)
 
-    %w[Default Profile\ 1 Profile\ 2].each do |profile|
-      profile_dir = File.join(support_dir, profile)
+    profiles.each do |directory, name|
+      profile_dir = File.join(support_dir, directory)
 
       FileUtils.mkdir_p(profile_dir)
 
       preferences = File.join(profile_dir, BraveAlfred::PREFERENCES_FILE)
 
       File.open(preferences, 'w+') do |file|
-        JSON.dump({ foo: 'bar' }, file)
+        JSON.dump(preference_content(name), file)
       end
     end
+  end
+
+  def preference_content(name)
+    {
+      profile: {
+        name: name
+      }
+    }
+  end
+
+  def test_profiles
+    @test_profiles ||= {
+      'Default' => 'Batman',
+      'Profile 1' => 'Robin',
+      'Profile 2' => 'Cat Woman',
+      'Guest Profile' => 'Guest',
+    }
   end
 end

a0a09158e5c523604ce1d0a9c40e6cf1e224eb9b
Albert Salim <albertlimca@gmail.com>
2022-05-23 14:57:29 +0800

    Refactor to generate items per profile

--- brave_alfred.rb
+++ brave_alfred.rb
@@ -10,6 +10,12 @@ module BraveAlfred
   LAUNCH = 'launch'
   INCOGNITO = 'incognito'
 
+  AlfredResponse = Struct.new(:items, keyword_init: true) do
+    def to_h
+      { items: items.map(&:to_h) }
+    end
+  end
+
   AlfredItem = Struct.new(:title, :subtitle, :arg, keyword_init: true)
 
   class Engine
@@ -18,29 +24,20 @@ module BraveAlfred
       @home = home
       @param = param
       @profiles = load_profiles
+      @item_factory = ItemFactory.new(command: command, param: param)
     end
 
     def execute
-      {
-        items: create_items.map(&:to_h)
-      }
+      AlfredResponse.new(items: create_items).to_h
     end
 
     private
 
-    attr_reader :home, :command, :param, :profiles
+    attr_reader :home, :command, :param, :profiles, :item_factory
 
     def create_items
-      return [] unless command == LAUNCH || command == INCOGNITO
-
-      profiles
-        .sort_by { |profile| profile.name }
-        .map do |profile|
-        AlfredItem.new(
-          title: profile.name,
-          subtitle: subtitle_for(profile),
-          arg: launcher_for(profile)
-        )
+      profiles.map do |profile|
+        item_factory.item_for(profile)
       end
     end
 
@@ -50,18 +47,7 @@ module BraveAlfred
       Dir.glob(glob_pattern)
          .reject { |path| path.include?('System Profile') }
          .map { |preference_path| Profile.from(preference_path) }
-    end
-
-    def subtitle_for(profile)
-      base = "Open Brave Browser as #{profile.name}"
-      base += " in private" if command == INCOGNITO
-      base
-    end
-
-    def launcher_for(profile)
-      base = "#{EXECUTABLE} #{param.nil? ? '' : "\"#{param}\" " }--profile-directory=\"#{profile.directory}\""
-      base += " --incognito" if command == INCOGNITO
-      base
+         .sort_by { |profile| profile.name }
     end
 
     Profile = Struct.new(:name, :directory, keyword_init: true) do
@@ -84,10 +70,75 @@ module BraveAlfred
       end
     end
   end
+
+  class ItemFactory
+    def initialize(command:, param:)
+      @command = command
+      @param = param
+    end
+
+    def item_for(profile)
+      item_class.new(profile: profile, param: param).item
+    end
+
+    private
+
+    def item_class
+      case command
+      when LAUNCH then Launch
+      when INCOGNITO then Incognito
+      else
+        raise ArgumentError
+      end
+    end
+
+    attr_reader :command, :param
+  end
+
+  class Launch
+    def initialize(profile:, param:)
+      @profile = profile
+      @param = param
+    end
+
+    def item
+      AlfredItem.new(
+        title: profile.name,
+        subtitle: subtitle,
+        arg: launcher
+      )
+    end
+
+    private
+
+    def subtitle
+      "Open Brave Browser as #{profile.name}"
+    end
+
+    def launcher
+      "#{EXECUTABLE} #{url}--profile-directory=\"#{profile.directory}\""
+    end
+
+    def url
+      param.nil? ? '' : "\"#{param}\" "
+    end
+
+    attr_reader :profile, :param
+  end
+
+  class Incognito < Launch
+    def subtitle
+      super + " in private"
+    end
+
+    def launcher
+      super + " --incognito"
+    end
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
   command = ARGV[0]
   param = ARGV[1]
-  puts BraveAlfred.new(command: command, param: param).execute.to_json
+  puts BraveAlfred::Engine.new(command: command, param: param).execute.to_json
 end
